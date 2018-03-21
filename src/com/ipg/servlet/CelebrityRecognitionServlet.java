a7d32f4f7e8a4de64c923b89723287bf515a7db5
Jeewan Jayathilake <efacjeewan@gmail.com>
2018-03-21 08:16:18 -0500

    Jeewan: Added celebrity recognition functionality

--- src/com/ipg/servlet/CelebrityRecognitionServlet.java
+++ src/com/ipg/servlet/CelebrityRecognitionServlet.java
@@ -1,11 +1,24 @@
 package com.ipg.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.BoundingBox;
+import com.amazonaws.services.rekognition.model.Celebrity;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.RecognizeCelebritiesRequest;
+import com.amazonaws.services.rekognition.model.RecognizeCelebritiesResult;
+import com.amazonaws.services.rekognition.model.S3Object;
+import com.ipg.listener.AmazonRekognitionListener;
+import com.ipg.util.AmazonS3UploaderUtil;
 
 /**
  * Servlet implementation class CelebrityRecognitionServlet
@@ -34,8 +47,36 @@ public class CelebrityRecognitionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String imageName = AmazonS3UploaderUtil.uploadToAmazonS3Bucket(request);
+		ServletContext context = request.getSession().getServletContext();
+		String bucket = AmazonRekognitionListener.getAmazonS3Bucket(context);
+		AmazonRekognition rekognitionClient = AmazonRekognitionListener.getRekognitionClient(context);
+		
+		RecognizeCelebritiesRequest celebDectRequest = new RecognizeCelebritiesRequest()
+				.withImage(new Image().withS3Object(new S3Object().withName(imageName).withBucket(bucket)));
+		
+		RecognizeCelebritiesResult result= rekognitionClient.recognizeCelebrities(celebDectRequest);
+		
+		////////////////////////////////////////////
+		//Display recognized celebrity information
+		
+        List<Celebrity> celebs=result.getCelebrityFaces();
+        System.out.println(celebs.size() + " celebrity(s) were recognized.\n");
 
+        for (Celebrity celebrity: celebs) {
+            System.out.println("Celebrity recognized: " + celebrity.getName());
+            System.out.println("Celebrity ID: " + celebrity.getId());
+            BoundingBox boundingBox=celebrity.getFace().getBoundingBox();
+            System.out.println("position: " +
+               boundingBox.getLeft().toString() + " " +
+               boundingBox.getTop().toString());
+            System.out.println("Further information (if available):");
+            for (String url: celebrity.getUrls()){
+               System.out.println(url);
+            }
+            System.out.println();
+         }
+         System.out.println(result.getUnrecognizedFaces().size() + " face(s) were unrecognized.");
+     }
+		///////////////////////////////////////////
 }
