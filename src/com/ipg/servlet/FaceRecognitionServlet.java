926bbcb33409b73853ee8e61adca952687f8118d
Jeewan Jayathilake <efacjeewan@gmail.com>
2018-03-21 10:25:57 -0500

    Tram:

--- src/com/ipg/servlet/FaceRecognitionServlet.java
+++ src/com/ipg/servlet/FaceRecognitionServlet.java
@@ -2,7 +2,6 @@ package com.ipg.servlet;
 
 import java.io.File;
 import java.io.IOException;
-import com.cloudinary.*;
 
 import java.io.PrintWriter;
 import java.util.Arrays;
@@ -18,11 +17,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import com.cloudinary.Cloudinary;
 
 
-import com.cloudinary.*;
-import com.cloudinary.utils.ObjectUtils;
+
+
+import com.ipg.util.DetectObject;
 
 
 
--- src/com/ipg/servlet/VehicleRecognitionServlet.java
+++ src/com/ipg/servlet/VehicleRecognitionServlet.java
@@ -13,11 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.cloudinary.Cloudinary;
-
-
-import com.cloudinary.*;
-import com.cloudinary.utils.ObjectUtils;
+import com.ipg.util.DetectObject;
 
 /**
  * Servlet implementation class VehicleRecognitionServlet
